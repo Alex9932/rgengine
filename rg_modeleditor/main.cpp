b916f14bc732f37a99c24891d1da558bc369fb4a
Alex9932 <alex9932@inbox.ru>
2025-04-03 06:01:41 +0300

    Added path parser in modeleditor

--- rg_modeleditor/main.cpp
+++ rg_modeleditor/main.cpp
@@ -1,14 +1,15 @@
 #define GAME_DLL
 #include <rgentrypoint.h>
 #include <event.h>
+#include <rgstring.h>
+#include <filedialog.h>
+#include <filesystem.h>
 #include <imgui/imgui_impl_sdl2.h>
 #include <imgui/imgui.h>
 
 #include "renderer.h"
 
 //#include <zlib.h>
-
-//#include <filesystem.h>
 //#include <allocator.h>
 
 using namespace Engine;
@@ -27,7 +28,7 @@ static void ProcessSFile(String file) {
 	size_t buff_len = 1024 * 1024 * 16;
 	void* out = rg_malloc(buff_len);
 
-	rgLogInfo(RG_LOG_GAME, "%.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x", h[0],  h[1],  h[2],  h[3],  h[4],  h[5],  h[6],  h[7],  h[8],  h[9],  h[10], h[11], h[12], h[13], h[14], h[15]);
+	rgLogInfo(RG_LOG_GAME, "%.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x", h[0], h[1], h[2], h[3], h[4], h[5], h[6], h[7], h[8], h[9], h[10], h[11], h[12], h[13], h[14], h[15]);
 	rgLogInfo(RG_LOG_GAME, "%.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x", h[16], h[17], h[18], h[19], h[20], h[21], h[22], h[23], h[24], h[25], h[26], h[27], h[28], h[29], h[30], h[31]);
 
 	if (h[16] == 0x78 && (h[17] == 0x9C || h[17] == 0xDA)) {
@@ -35,9 +36,9 @@ static void ProcessSFile(String file) {
 
 		z_stream zstr = {};
 
-		zstr.next_in   = (Bytef*)&h[16];
-		zstr.avail_in  = res->length - 16;
-		zstr.next_out  = (Bytef*)out;
+		zstr.next_in = (Bytef*)&h[16];
+		zstr.avail_in = res->length - 16;
+		zstr.next_out = (Bytef*)out;
 		zstr.avail_out = buff_len;
 
 		if (inflateInit(&zstr) != Z_OK) {
@@ -79,19 +80,56 @@ static Bool CEventHandler(SDL_Event* event) {
 
 static char MDL_NAME[512];
 static char MDL_PATH[512];
+static char MDL_EXT[32];
 
-static void LoadModel(String path) {
-	// Parse path
-	// Save %MODELNAME% in MDL_NAME and %PATH% in MDL_PATH
-	// Path example: /%PATH%/%MODELNAME%.%EXT%
-	// /home/alex9932/rgengine/rawmodels/cube/cube.obj
-	// - %PATH% -> /home/alex9932/rgengine/rawmodels/cube
-	// - %MODELNAME% -> cube
-	// - %EXT% -> obj
+static void LoadModel() {
+
 }
 
 static void OpenModel() {
 	// Open filedialog
+	char raw_path[512] = {};
+	char path[512] = {};
+	FD_Filter filters[4] = { {"Wavefront model", "obj"}, {"PM2 Model file", "pm2"}, {"MMD Polygon model", "pmd"}, {"MMD Extended polygon model", "pmx"} };
+	if (ShowOpenDialog(raw_path, 512, filters, 4)) {
+		FS_ReplaceSeparators(path, raw_path);
+		rgLogInfo(RG_LOG_SYSTEM, ":> %s", path);
+
+		// Parse path
+		// Save %MODELNAME% in MDL_NAME and %PATH% in MDL_PATH
+		// Path example: /%PATH%/%MODELNAME%.%EXT%
+		// /home/alex9932/rgengine/rawmodels/cube/cube.obj
+		// - %PATH% -> /home/alex9932/rgengine/rawmodels/cube
+		// - %MODELNAME% -> cube
+		// - %EXT% -> obj
+
+		// /blah-blah-blah/cube.obj
+		//             sep^ ext^
+		Sint32 sep = rg_strcharate(path, '/'); // separator
+		Sint32 ext = rg_strcharate(path, '.'); // extension
+		Uint32 len = SDL_strlen(path);
+
+		if ((len - ext) - 1 >= 32) {
+			rgLogError(RG_LOG_SYSTEM, "Invalid filename!");
+			return;
+		}
+
+		// Clear buffers
+		SDL_memset(MDL_PATH, 0, 512);
+		SDL_memset(MDL_NAME, 0, 512);
+		SDL_memset(MDL_EXT, 0, 32);
+
+		// Copy data
+		SDL_memcpy(MDL_PATH, path, sep); // path w/o filename
+		SDL_memcpy(MDL_NAME, &path[sep + 1], len - sep - (len - ext) - 1); // filename w/o extension
+		SDL_memcpy(MDL_EXT, &path[ext + 1], len - ext - 1); // filename w/o extension
+
+		rgLogInfo(RG_LOG_SYSTEM, "-> %s", MDL_PATH);
+		rgLogInfo(RG_LOG_SYSTEM, "-> %s", MDL_NAME);
+		rgLogInfo(RG_LOG_SYSTEM, "-> %s", MDL_EXT);
+
+		LoadModel();
+	}
 }
 
 static void SaveModel() {
@@ -115,42 +153,42 @@ static void DrawGUI() {
 
 	ImGui::Text("Loaded model: %s", MDL_NAME);
 	ImGui::Text("Path: %s", MDL_PATH);
-	
+
 	ImGui::End();
 }
 
 class Application : public BaseGame {
-	public:
-		Application() {
-			this->isClient   = true;
-			this->isGraphics = false;
-		}
-		~Application() {}
+public:
+	Application() {
+		this->isClient = true;
+		this->isGraphics = false;
+	}
+	~Application() {}
 
-		String GetName() { return "Model importer"; }
+	String GetName() { return "Model importer"; }
 
-		void MainUpdate() {
-			DoRender(rstate);
-		}
+	void MainUpdate() {
+		DoRender(rstate);
+	}
 
-		void Initialize() {
-			rstate = InitializeRenderer(DrawGUI);
-			RegisterEventHandler(CEventHandler);
+	void Initialize() {
+		rstate = InitializeRenderer(DrawGUI);
+		RegisterEventHandler(CEventHandler);
 
-			//ProcessSFile("E:/.../TRAINS/TRAINSET/rz_VL10-1487/rz_vl10-1487b.s");
-			//ProcessSFile("E:/.../TRAINS/TRAINSET/tsrLoco_VL8-1718/vl8-1718a.s");
-		
+		//ProcessSFile("E:/.../TRAINS/TRAINSET/rz_VL10-1487/rz_vl10-1487b.s");
+		//ProcessSFile("E:/.../TRAINS/TRAINSET/tsrLoco_VL8-1718/vl8-1718a.s");
 
-		}
 
-		void Quit() {
-			DestroyRenderer(rstate);
-			FreeEventHandler(CEventHandler);
-		}
+	}
 
-	private:
+	void Quit() {
+		DestroyRenderer(rstate);
+		FreeEventHandler(CEventHandler);
+	}
 
-		RenderState* rstate = NULL;
+private:
+
+	RenderState* rstate = NULL;
 
 };
 
